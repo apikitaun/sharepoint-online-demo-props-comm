2f8e72a6c5e947d7e6d3e38bd33e0f6b1ba26dd7
Ubuntu Development <jorge.gcia.gcia@gmail.com>
2017-10-29 05:50:14 -0700

    Demo + Properties

---
+++ src/webparts/commTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICommTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CommTestWebPartStrings' {
+  const strings: ICommTestWebPartStrings;
+  export = strings;
+}
